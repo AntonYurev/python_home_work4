bcdadc5d091bb6831ddb8fca30dca250cf3a04cd
Anton <altay77@inbox.ru>
2022-11-07 23:37:59 +0700

    solution1

---
+++ number4.py
@@ -0,0 +1,7 @@
+# Вычислить число c заданной точностью d (10^{-1} ≤ d ≤10^{-10})
+# import random
+# n = random.random()*(random.randint(1, 20))
+# print ('Наше число: ', n)
+# d = str(input('Введите точность округления: '))
+# d = d.split('.')
+# print ('Вычисленное число: ', round(n, len(d[1])))
